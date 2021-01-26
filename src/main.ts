c5bd9041b4272be5a9d12c77af50fcde5e008624
陆佳鑫 <lujiaxin@192.168.0.110>
2021-01-26 22:23:28 +0800

    commit on master

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.css'
-import {createWebHashHistory,createRouter} from 'vue-router'
+import './index.scss'
+import {createWebHashHistory, createRouter} from 'vue-router'
 import Home from './views/Home.vue'
 import Doc from './views/Doc.vue'
 
 const history = createWebHashHistory()
 const router = createRouter({
-    history:history,
-    routes:[
-        {
-            path:'/',
-            component:Home
-        },
-        {
-            path:'/doc',
-            component:Doc
-        }
+    history: history,
+    routes: [
+        {path: '/', component: Home},
+        {path: '/doc', component: Doc}
     ]
 })
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,22 @@
 <template>
-    <div>
-        <div class="topnav">
-            <div class="logo"></div>
-            <div class="menu"></div>
-        </div>
-        <div class="banner">
-            <h1>轱辘UI</h1>
-            <h2>一个厉害的UI框架</h2>
-            <p class="actions">
-                <a>Github</a>   
-                <a>开始</a>
-            </p>
-        </div>
+  <div>
+    <Topnav/>
+    <div class="banner">
+      <h1>轱辘UI</h1>
+      <h2>一个厉害的UI框架</h2>
+      <p class="actions">
+        <a>Github</a>
+        <a>开始</a>
+      </p>
     </div>
+  </div>
 </template>
 <style lang="scss" scoped>
-    .topnav{
-        border: 1px solid red;
-    }
+
 </style>
+<script lang="ts">
+import Topnav from "../components/Topnav";
+export default {
+  components: {Topnav}
+}
+</script>
